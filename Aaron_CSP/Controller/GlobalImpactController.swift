9c3593e8a552c7dec44011231b5568e95c7b7375
Aaron Montoya <aatoya7@gmail.com>
2017-11-10 13:45:58 -0700

    Added a page and started working on it. Three labels and two images.

--- Aaron_CSP/Controller/GlobalImpactController.swift
+++ Aaron_CSP/Controller/GlobalImpactController.swift
@@ -12,7 +12,9 @@ class GlobalImpactController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadLabels()
+        loadImage()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,5 +22,22 @@ class GlobalImpactController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Outlets
+    @IBOutlet weak var nuclearImage: UIImageView!
+    @IBOutlet weak var planLabel: UILabel!
+    @IBOutlet weak var personalLabel: UILabel!
+    
+    //Load the image and text programmatically
+    public func loadImage() -> Void
+    {
+        nuclearImage.image = #imageLiteral(resourceName: "GlobalImpactImage2")
+    }
+    
+    public func loadLabels()
+    {
+        planLabel.text = "The Plan for the app"
+        personalLabel.text = "Personal connection to the global problem"
+    }
+    
 }

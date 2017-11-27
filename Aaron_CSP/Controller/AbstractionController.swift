2b9cb6c10dc3f3811112d341f30bfb9e1ebdb2a9
Aaron Montoya <aatoya7@gmail.com>
2017-11-27 12:12:10 -0700

    Added orderedAbstractionViews, newAbstractionViewController, and pageViewController methods to the AbstractionController class.

--- Aaron_CSP/Controller/AbstractionController.swift
+++ Aaron_CSP/Controller/AbstractionController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class AbstractionController: UIViewController {
+class AbstractionController: UIViewController, UIPageViewControllerDataSource {
+    
+    //MARK: Array of subviews
+    private (set) lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate")
+        ]
+    }()
+    
+    //Helper methods to retrieve the correct ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +34,12 @@ class AbstractionController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //MARK:- Required Protocol methods for UIPageViewControllerDatacource
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

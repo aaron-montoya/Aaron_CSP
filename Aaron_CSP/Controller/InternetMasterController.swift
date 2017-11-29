e62c186fe7291b7bd131cf9c51a15964227453a7
Aaron Montoya <aatoya7@gmail.com>
2017-11-29 11:52:49 -0700

    Finished InternetMasterController.

---
+++ Aaron_CSP/Controller/InternetMasterController.swift
@@ -0,0 +1,56 @@
+//
+//  InternetMasterController.swift
+//  Aaron_CSP
+//
+//  Created by Montoya, Aaron on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterController: UITableViewController
+{
+
+    private (set) lazy var internetTopics : [String] =
+    {
+        return [
+            "Definitions",
+            "CSP",
+            "CTEC",
+            "Canyons",
+            "Twitter",
+            "Swift Guide"
+        ]
+    }()
+    
+    private var detailViewController : InternetDetailViewController?
+    
+    private func setup() -> Void
+    {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        self.clearsSelectionOnViewWillAppear = false
+    }
+
+    //MARK: - Table view data source
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of selections
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        
+        return cell
+    }
+
+}

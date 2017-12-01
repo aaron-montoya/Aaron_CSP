e434cb9b2cdfd6587a6a2f7203d19359ce770f4c
Aaron Montoya <aatoya7@gmail.com>
2017-12-01 13:42:19 -0700

    Worked on InternetMasterController.

--- Aaron_CSP/Controller/InternetMasterController.swift
+++ Aaron_CSP/Controller/InternetMasterController.swift
@@ -38,7 +38,16 @@ class InternetMasterController: UITableViewController
             "https://www.google.com",
             "https://www.google.com"
         ]
+        
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            detailViewController = currentControllers[0] as?
+            InternetDetailViewController
+        }
     }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,5 +71,54 @@ class InternetMasterController: UITableViewController
         
         return cell
     }
+    
+    //MARK: Handle the internal transfer
+    override public func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier! == "showDetail"
+        {
+            if let indexPath = self.tableView.indexPathForSelectedRow
+            {
+                let urlString = addresses[indexPath.row]
+                let pageText : String
+                
+                if indexPath.row == 0
+                {
+                    //TODO: Replace with my definitions - great time to use the """ operator
+                    pageText = "All the definitions you wrote..."
+                }
+                else
+                {
+                    pageText = internetTopics[indexPath.row]
+                }
+                
+                let controller = segue.destination as! InternetDetailViewController
+                
+                controller.detailAddress = urlString
+                controller.detailText = pageText
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

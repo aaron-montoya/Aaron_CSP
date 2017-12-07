00d4dc23e1186edd34e6a4014fbb07916ff6a4ed
Aaron Montoya <aatoya7@gmail.com>
2017-12-07 11:45:23 -0700

    Started writing the CreativityController class.

--- Aaron_CSP/View/DrawingView.swift
+++ Aaron_CSP/View/DrawingView.swift
@@ -21,19 +21,19 @@ class DrawingView: UIView {
     {
         //Drawing code
         drawStickFigure().stroke()
-        //drawTurtle()
-        //drawHappyTree()
+        drawTurtle()
+        drawHappyTree()
     }
     
     private func drawStickFigure() -> UIBezierPath
     {
         let stickFigure = UIBezierPath()
         
-        UIColor.magenta.setStroke()
+        UIColor.yellow.setStroke()
         stickFigure.lineWidth = 3.0
         
         stickFigure.addArc(withCenter: CGPoint(x: 200, y: 200),
-                           radius: CGFloat(20),
+                           radius: CGFloat(32),
                            startAngle: CGFloat(0),
                            endAngle: CGFloat(2) * CGFloat.pi,
                            clockwise: true)
@@ -47,6 +47,47 @@ class DrawingView: UIView {
         stickFigure.addLine(to: CGPoint(x: 220, y: 300))
         
         return stickFigure
+    }
+    
+    private func drawHappyTree()
+    {
+        let bobRoss = UIBezierPath()
+        
+        bobRoss.move(to: CGPoint(x: 80, y:50))
+        bobRoss.addLine(to: CGPoint(x:120, y:150))
+        bobRoss.addLine(to: CGPoint(x:40, y:150))
+        bobRoss.close()
+        UIColor(patternImage: UIImage(named: "beat")!).setFill()
+        UIColor.green.setStroke()
+        bobRoss.lineWidth = 2.0
+        bobRoss.fill()
+        bobRoss.stroke()
+        
+        let happyTree = UIBezierPath()
+        
+        UIColor(patternImage: UIImage(named: "beat")!).setFill()
+        happyTree.move(to: CGPoint(x: 110, y: 150))
+        happyTree.addLine(to:CGPoint(x: 150, y: 200))
+        happyTree.addLine(to: CGPoint(x:10, y:200))
+        happyTree.addLine(to:CGPoint(x:50, y:150))
+        happyTree.close()
+        happyTree.stroke()
+        happyTree.fill()
+        happyTree.move(to: CGPoint(x:80, y:200))
+        happyTree.addLine(to: CGPoint(x:80, y:250))
+        happyTree.lineWidth = 2.0
+        happyTree.stroke()
+    }
+    
+    public func drawTurtle() -> Void
+    {
+        let logo = UIBezierPath()
+        UIColor.white.setFill()
+        logo.move(to: CGPoint(x: 50, y: 250))
+        logo.addLine(to: CGPoint(x: 100, y: 300))
+        logo.addLine(to: CGPoint(x: 50, y: 350))
+        logo.close()
+        logo.fill()
     }
 
 

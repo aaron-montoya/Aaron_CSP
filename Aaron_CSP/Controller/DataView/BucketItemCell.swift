7e142ba5e7c0d5f2f20c83d40da60179a8678081
Aaron Montoya <aatoya7@gmail.com>
2018-01-09 12:44:00 -0700

    Working on BucketItemCell Class.

--- Aaron_CSP/Controller/DataView/BucketItemCell.swift
+++ Aaron_CSP/Controller/DataView/BucketItemCell.swift
@@ -14,6 +14,29 @@ class BucketItemCell: UITableViewCell {
     @IBOutlet weak var bucketItemSymbol : UILabel!
     @IBOutlet weak var bucketItemSignature : UILabel!
     
+    var bucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
+    }
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+        let symbolRange = 79
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
